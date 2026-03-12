804a4ad8929f84e060c33f91a56064048ea27ccb
NRZ-10 <niranjan.zr710@gmail.com>
2026-03-12 13:55:51 +0530

    update

--- Aofshapes.java
+++ Aofshapes.java
@@ -1,40 +1,61 @@
 import java.util.Scanner;
 
-class AreaShapes {
+class Aofshapes {
 
-    // Area of Circle
+    // area of circle
     double area(double r) {
         return 3.14 * r * r;
     }
 
-    // Area of Rectangle
+    // area of rectangle
     double area(double l, double b) {
         return l * b;
     }
 
-    // Area of Triangle
-    double area(double b, double h, int x) {
+    // area of triangle
+    double area(float b, float h) {
         return 0.5 * b * h;
     }
 
-    public static void Aofshapes(String[] args) {
+    public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
-        AreaShapes obj = new AreaShapes();
+        Aofshapes obj = new Aofshapes();
 
-        System.out.println("Enter radius of circle:");
-        double r = sc.nextDouble();
-        System.out.println("Area of Circle = " + obj.area(r));
+        System.out.println("1. Circle");
+        System.out.println("2. Rectangle");
+        System.out.println("3. Triangle");
 
-        System.out.println("Enter length and breadth of rectangle:");
-        double l = sc.nextDouble();
-        double b = sc.nextDouble();
-        System.out.println("Area of Rectangle = " + obj.area(l, b));
+        System.out.print("Enter your choice: ");
+        int ch = sc.nextInt();
 
-        System.out.println("Enter base and height of triangle:");
-        double base = sc.nextDouble();
-        double height = sc.nextDouble();
-        System.out.println("Area of Triangle = " + obj.area(base, height, 1));
+        switch(ch) {
+
+            case 1:
+                System.out.print("Enter radius: ");
+                double r = sc.nextDouble();
+                System.out.println("Area of Circle = " + obj.area(r));
+                break;
+
+            case 2:
+                System.out.print("Enter length: ");
+                double l = sc.nextDouble();
+                System.out.print("Enter breadth: ");
+                double b = sc.nextDouble();
+                System.out.println("Area of Rectangle = " + obj.area(l,b));
+                break;
+
+            case 3:
+                System.out.print("Enter base: ");
+                float base = sc.nextFloat();
+                System.out.print("Enter height: ");
+                float h = sc.nextFloat();
+                System.out.println("Area of Triangle = " + obj.area(base,h));
+                break;
+
+            default:
+                System.out.println("Invalid choice");
+        }
 
         sc.close();
     }
